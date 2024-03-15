43337098ae7b9e323f4da02b40a22f9d7fffef26
unknown <mdomarfaruk149518@gmail.com>
2024-03-15 12:00:17 +0600

    [Solved]Problem_4

---
+++ problem_4.js
@@ -0,0 +1,19 @@
+const maxProfit = (prices) => {
+  if (!prices || prices.length <= 1) {
+    return 0;
+  }
+
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i]);
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+  }
+
+  return maxProfit;
+};
+
+const result = maxProfit([7, 1, 5, 3, 6, 4]);
+
+console.log(result);
