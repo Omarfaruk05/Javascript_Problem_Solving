6ec7c100a10b36b836d3af4306b94383b837f95c
unknown <mdomarfaruk149518@gmail.com>
2024-03-15 12:35:11 +0600

    [Solved]Problem_7

---
+++ problem_7.js
@@ -0,0 +1,9 @@
+// find index of an array for my dadicate value
+
+const search = (nums, target) => {
+  return nums.indexOf(target);
+};
+
+const result = search([4, 5, 6, 7, 0, 1, 2], 9);
+
+console.log(result);
